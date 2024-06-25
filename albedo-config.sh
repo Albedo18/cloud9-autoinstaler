144a08c4d63c0c4b59ff7243b506877159f71fa8
Albedo18 <hamedotamvan@icloud.com>
2024-06-25 12:33:29 +0700

    Update albedo-config.sh

--- albedo-config.sh
+++ albedo-config.sh
@@ -11,5 +11,5 @@ echo -e "${A}--[ AUTO CONFIG ]--${C}"
 echo -e "${D}"
 
 z="
-";ABz=' x/s';Nz='E';Jz='9):"';Ez='aru:';FBz=' -a ';pz='DER';xz='!"';Wz='D';ez='LDER';HBz='E:$P';EBz='$POR';CBz='r.js';Mz='RNAM';bz=' POR';Cz='ERNA';Rz=' (pa';gz='ud9:';dz=' "FO';lz=' tul';DBz=' -p ';Zz=' (po';Pz='SSWO';Oz=' "PA';KBz='-w /';Fz=' (us';iz='o ga';Xz=' "PO';qz=' "CL';vz='I NJ';kz='erti';Bz=' "US';GBz='$USE';Yz='RT b';Vz='SWOR';MBz='/ALB';Uz=' PAS';LBz='home';sz=' SIA';oz=' FOL';hz='(kal';IBz='ASSW';Tz='rd c';Iz='loud';Sz='sswo';JBz='ORD ';wz='ING!';NBz='EDO-';az='rt c';Gz='erna';BBz='erve';yz='node';rz='OUD9';uz='PAKA';Kz='read';mz='is a';Az='echo';Lz=' USE';nz='ja c';Hz='me c';jz='k ng';Dz='ME b';cz='T_C9';OBz='$FOL';tz='P DI';fz=' clo';Qz='RD b';
-eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$z$Kz$Lz$Mz$Nz$z$Az$Oz$Pz$Qz$Ez$Rz$Sz$Tz$Iz$Jz$z$Kz$Uz$Vz$Wz$z$Az$Xz$Yz$Ez$Zz$az$Iz$Jz$z$Kz$bz$cz$z$Az$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$Iz$Jz$z$Kz$oz$pz$z$Az$qz$rz$sz$tz$uz$vz$wz$xz$z$yz$ABz$BBz$CBz$DBz$EBz$cz$FBz$GBz$Mz$HBz$IBz$JBz$KBz$LBz$MBz$NBz$OBz$pz"
+";mz=' clo';iz=' POR';QBz=':$PA';Gz='erna';uz='ja c';jz>
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$z$Kz$Lz$Mz$Nz$z$Az>
