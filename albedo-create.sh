f8181142c33d0d74188e33803be93d96f1db9b27
Albedo18 <hamedotamvan@icloud.com>
2024-06-25 17:43:36 +0700

    Update and rename albedo-config.sh to albedo-create.sh

---
+++ albedo-create.sh
@@ -0,0 +1,13 @@
+A="\e[31m"
+B="\e[1;${A}"
+C="\e[0m"
+D="\e[34m"
+echo ""
+echo ""
+echo -e "${B}--[ ALBEDO CLOUD9 STORE ]--${C}"
+echo ""
+echo -e "${A}  --[ CREATE ACCOUNT ]--${C}"
+echo -e "${D}"
+z="
+";ez=' "PO';HBz='p 5';Xz=' "IP';DCz='FOLD';uz='ja c';iz=' POR';BBz='NG! ';Ez='aru:';tz='is a';XBz='AME"';Dz='ME b';hz='rt c';Fz=' (us';kz=' "FO';Qz='RD b';bBz='ASSW';uBz='PASS';MBz='ING!';sz=' tul';xz=' "TU';FBz='!"';Cz='ERNA';rz='erti';FCz=' /de';HCz='ll 2';CCz='do-$';xBz='/hom';pz='o ga';IBz=' ""';hBz='INAL';Kz='read';ACz='AME/';YBz='"';GBz='slee';fz='RT b';eBz='OSE ';PBz='tp:/';BCz='albe';Az='echo';nBz='p $P';VBz=' "$U';lz='LDER';oBz='ORT_';OBz=' "ht';Nz='E';dz=' IP';Lz=' USE';Yz=' : (';sBz=' $US';QBz='/"$I';Iz='loud';jBz='node';qBz='l $I';cBz='ORD"';GCz='v/nu';RBz='P":"';vBz='WORD';CBz='MASI';yz='NGGU';cz='r):"';UBz='ME :';mz=' clo';gBz='TERM';yBz='e/$U';TBz='""';NBz='p 2';Rz=' (pa';Bz=' "US';lBz='ver.';Vz='SWOR';jz='T_C9';ECz='ER >';bz='erve';KBz='LESA';mBz='js -';Tz='rd c';ICz='>&1';wz='DER';az='ps/s';qz='k ng';Mz='RNAM';Pz='SSWO';dBz=' "CL';SBz='$POR';oz='(kal';gz=' (po';LBz='I NJ';Jz='9):"';DBz='H PR';aBz=' "$P';fBz='AJA ';JBz=' "SE';nz='ud9:';Wz='D';Gz='erna';WBz='SERN';Uz=' PAS';wBz=' -w ';Zz='ip v';ZBz='RD :';pBz='C9 -';Hz='me c';ABz=' NJI';iBz='MU J';vz=' FOL';EBz='OSES';rBz='P -a';tBz='ME:$';Oz=' "PA';Sz='sswo';kBz=' ser';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$z$Kz$Lz$Mz$Nz$z$Az$Oz$Pz$Qz$Ez$Rz$Sz$Tz$Iz$Jz$z$Kz$Uz$Vz$Wz$z$Az$Xz$Yz$Zz$az$bz$cz$z$Kz$dz$z$Az$ez$fz$Ez$gz$hz$Iz$Jz$z$Kz$iz$jz$z$Az$kz$lz$mz$nz$oz$pz$qz$rz$sz$tz$uz$Iz$Jz$z$Kz$vz$wz$z$Az$xz$yz$ABz$BBz$CBz$DBz$EBz$FBz$z$GBz$HBz$z$Az$IBz$z$Az$JBz$KBz$LBz$MBz$FBz$z$GBz$NBz$z$Az$IBz$z$Az$OBz$PBz$QBz$RBz$SBz$jz$TBz$z$Az$IBz$z$Az$Bz$Cz$UBz$VBz$WBz$XBz$YBz$z$Az$IBz$z$Az$Oz$Pz$ZBz$aBz$bBz$cBz$YBz$z$Az$IBz$z$Az$dBz$eBz$fBz$gBz$hBz$iBz$MBz$FBz$z$jBz$kBz$lBz$mBz$nBz$oBz$pBz$qBz$rBz$sBz$Cz$tBz$uBz$vBz$wBz$xBz$yBz$WBz$ACz$BCz$CCz$DCz$ECz$FCz$GCz$HCz$ICz"
